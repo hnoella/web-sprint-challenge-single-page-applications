cd7a87e042c19777602454cb7709c1518368d7bb
Noella Hirwa <noellahirwa@Noellas-MacBook-Pro.local>
2021-10-15 19:44:07 +0200

    commiting app.js and home.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import Home from "./components/Home.js";
+import Form from "./components/Form.js";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="App">
+      <nav>
+        <h1> Pizza Pizza</h1>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="#">Help</Link>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/form">
+          <Form />
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+
+import Form from "./Form";
+
+function Home() {
+  return (
+    <div>
+      <div>
+        <h1>Lambda Pizza</h1>
+      </div>
+      <div id="Pizza">
+        <img src="./pizza.jpeg" alt="this is pizza" />
+        <div className="Button">
+          <Router>
+            <button className="move">
+              {" "}
+              <Link id="order-pizza" to="/pizza">
+                order-Pizza
+              </Link>{" "}
+            </button>
+            <Switch>
+              <Route exact path="/pizza">
+                <Form />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
